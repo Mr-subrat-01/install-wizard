16412b830031dbccff7f395b69eb6988c39c6973
Mr-subrat-01 <skumarsahoo470@gmail.com>
2025-03-17 10:24:38 +0530

    form new version added

---
+++ src/app/form-builder/form-builder.component.ts
@@ -0,0 +1,97 @@
+import { CommonModule, NgFor, NgIf, TitleCasePipe } from '@angular/common';
+import {
+  Component,
+  EventEmitter,
+  Input,
+  NgModule,
+  Output,
+} from '@angular/core';
+import { FormsModule, NgModel } from '@angular/forms';
+import { NgSelectModule } from '@ng-select/ng-select';
+
+@Component({
+  selector: 'app-form-builder',
+  standalone: true,
+  imports: [
+    NgIf,
+    CommonModule,
+    FormsModule,
+    NgFor,
+    TitleCasePipe,
+    NgSelectModule,
+  ],
+  templateUrl: './form-builder.component.html',
+  styleUrl: './form-builder.component.css',
+})
+export class FormBuilderComponent {
+  @Input() isOpen: boolean = false; // Control modal from Parent
+  @Output() closeModal = new EventEmitter<void>(); // Notify Parent to close
+  @Output() formJsonGenerated = new EventEmitter<any>(); // Emit JSON
+
+  formFields: any[] = [];
+  selectedType: string = '';
+  formTypes = ['text', 'checkbox', 'radio', 'file', 'select', 'textarea'];
+  generatedJson: any = {};
+  allowed_extensions = ['jpg', 'jpeg', 'png', 'pdf', 'docx', 'doc'];
+  dataTypes = [
+    { label: 'Number', value: 'number' },
+    { label: 'Text', value: 'text' },
+    { label: 'Decimal', value: 'decimal' },
+    { label: 'Boolean', value: 'boolean' },
+    { label: 'Date', value: 'date' },
+    { label: 'Timestamp', value: 'timestamp' },
+    { label: 'JSON', value: 'json' },
+    { label: 'Email', value: 'email' },
+  ];
+  addField(type: string) {
+    let newField: any = {
+      name: '',
+      label: '',
+      is_required: false,
+      type: type,
+      options: [],
+      extensions: [],
+      data_type: "",
+    };
+
+    if (type === 'file') {
+      newField.extensions = [];
+    }
+
+    if (type === 'radio' || type === 'checkbox' || type === 'select') {
+      newField.options = [''];
+    }
+
+    this.formFields.push(newField);
+  }
+
+  addOption(field: any) {
+    field.options.push('');
+  }
+
+  removeOption(field: any, index: number) {
+    field.options.splice(index, 1);
+  }
+
+  generateJson() {
+    this.generatedJson = {};
+    this.formFields.forEach((field) => {
+      const field_lower = field.label.toLowerCase().replace(/\s+/g, '_');
+      this.generatedJson[field_lower] = {
+        name: field_lower,
+        label: field.label,
+        is_required: field.is_required ? 'required' : '',
+        extensions: field.extensions,
+        options: field.options,
+        type: field.type,
+        data_type: field.data_type,
+      };
+    });
+
+    this.formJsonGenerated.emit(this.generatedJson);
+    // this.formFields = [];
+  }
+  removeField(index: number) {
+    this.formFields.splice(index, 1);
+  }
+}
--- src/app/workflow-form/workflow-form.component.ts
+++ src/app/workflow-form/workflow-form.component.ts
@@ -1,4 +1,4 @@
-import { NgFor } from '@angular/common';
+import { KeyValuePipe, NgFor } from '@angular/common';
 import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
 import {
@@ -6,21 +6,31 @@ import {
   FormBuilder,
   FormControl,
   FormGroup,
+  FormsModule,
   ReactiveFormsModule,
   Validators,
 } from '@angular/forms';
 import { NgSelectModule } from '@ng-select/ng-select';
+import { FormBuilderComponent } from "../form-builder/form-builder.component";
 
 @Component({
   selector: 'app-workflow-form',
   standalone: true,
-  imports: [ReactiveFormsModule, NgFor, NgSelectModule],
+  imports: [
+    ReactiveFormsModule,
+    NgFor,
+    NgSelectModule,
+    FormBuilderComponent,
+    FormsModule,
+    KeyValuePipe,
+  ],
   templateUrl: './workflow-form.component.html',
   styleUrl: './workflow-form.component.css',
 })
 export class WorkflowFormComponent {
   workflowForm: FormGroup;
   step = 0;
+  isRuleDialogOpen = false;
   wantToDelete = false;
   currentIndex: number = 0;
   userList = [
@@ -29,6 +39,25 @@ export class WorkflowFormComponent {
     { id: 3, name: 'Michael Brown' },
     { id: 4, name: 'Alice Johnson' },
   ];
+  conditions: any[] = [];
+  isFormModalOpen = false;
+  formJson: Record<
+    string,
+    {
+      name: string;
+      label: string;
+      is_required: string;
+      options: any[];
+      extensions: any[];
+      type: string;
+      data_type: string;
+    }
+  > = {};
+  fields: any = [];
+  rules: any[] = [];
+  ruleName: string = '';
+  ruleDescription: string = '';
+
   constructor(private fb: FormBuilder, private http: HttpClient) {
     this.workflowForm = this.fb.group({
       workflow: this.fb.group({
@@ -106,6 +135,12 @@ export class WorkflowFormComponent {
     return this.getStageFormGroup(stageIndex).get('tasks') as FormArray;
   }
 
+  getConditions(stageIndex: number, taskIndex: number): FormArray {
+    return this.getTaskFormGroup(stageIndex, taskIndex).get(
+      'conditions'
+    ) as FormArray;
+  }
+
   addTask(stageIndex: number) {
     const task = this.fb.group({
       name: ['', [Validators.required, Validators.maxLength(255)]],
@@ -122,6 +157,7 @@ export class WorkflowFormComponent {
       ],
       sla: ['', Validators.required],
       user_ids: this.fb.control([]),
+      conditions: this.fb.array([]),
     });
 
     this.getTasks(stageIndex).push(task);
@@ -136,5 +172,105 @@ export class WorkflowFormComponent {
     return this.getTaskFormGroup(stageIndex, taskIndex).get(
       'user_ids'
     ) as FormControl;
+  }
+
+  openFormModal() {
+    this.isFormModalOpen = true;
+  }
+
+  closeFormModal() {
+    this.isFormModalOpen = false;
+  }
+
+  onJsonGenerated(json: any) {
+    this.formJson = json;
+    console.log(this.formJson);
+    this.fields = Object.keys(this.formJson);
+    this.closeFormModal();
+  }
+  closeRuleDialog() {
+    this.isRuleDialogOpen = false;
+  }
+
+  openRuleDialog() {
+    this.isRuleDialogOpen = true;
+  }
+  getObjectKeys(obj: any): string[] {
+    return Object.keys(obj);
+  }
+
+  addRule() {
+    if (!this.ruleName.trim() || !this.ruleDescription.trim()) {
+      alert('Please enter Rule Name and Description.');
+      return;
+    }
+
+    const newRule = {
+      id: Math.floor(1000 + Math.random() * 9000),
+      name: this.ruleName,
+      description: this.ruleDescription,
+    };
+
+    this.rules.push(newRule);
+
+    this.ruleName = '';
+    this.ruleDescription = '';
+
+    console.log('Rules:', this.rules);
+    this.isRuleDialogOpen = false;
+  }
+  operators = ['=', '!=', '>', '<', '>=', '<='];
+
+  addCondition(stageIndex: number, taskIndex: number) {
+    const condition = this.fb.group({
+      field: ['', Validators.required],
+      operator: ['', Validators.required],
+      value: ['', Validators.required],
+      inputType: ['text'],
+    });
+
+    this.getConditions(stageIndex, taskIndex).push(condition);
+  }
+
+  onFieldChange(
+    event: Event,
+    stageIndex: number,
+    taskIndex: number,
+    condIndex: number
+  ) {
+    const selectedFieldName = (event.target as HTMLSelectElement).value;
+    const selectedField = this.formJson[selectedFieldName];
+    console.log(selectedField);
+    if (selectedField) {
+      const condition = this.getConditions(stageIndex, taskIndex).at(condIndex);
+      condition.patchValue({
+        inputType: this.getInputType(selectedField.data_type),
+      });
+    }
+  }
+
+  getInputType(dataType: string): string {
+    switch (dataType) {
+      case 'number':
+      case 'decimal':
+        return 'number';
+      case 'boolean':
+        return 'checkbox';
+      case 'date':
+        return 'date';
+      case 'timestamp':
+        return 'datetime-local';
+      case 'email':
+        return 'email';
+      case 'json':
+        return 'text';
+      default:
+        return 'text';
+    }
+  }
+
+  // Remove a condition
+  removeCondition(stageIndex: number, taskIndex: number, condIndex: number) {
+    this.getConditions(stageIndex, taskIndex).removeAt(condIndex);
   }
 }
